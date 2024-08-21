60189109d13802a2c40051caf99aef7fdc113393
Francisco Rodrigues <chicofariasneto@gmail.com>
2024-08-21 16:04:09 -0300

    run lint fix

--- src/core/utils/message.ts
+++ src/core/utils/message.ts
@@ -1,13 +1,13 @@
 import Movie from '../../domain/movie/model/movie';
 
 export const createMessageMovies = (movies: Array<Movie>): Array<string> => {
-  let listMessages: Array<string> = [];
+  const listMessages: Array<string> = [];
   listMessages.push('@here **CINE AMONG**\n**Lista de Filmes:**\n');
   let count = 1;
   for (const movie of movies) {
-    let title = `**${count++} - ${movie.title}**\n`;
-    let description = `**Descrição:** ${movie.overview}\n`;
-    let vote = `**Nota TMDB:** ${movie.voteAverage}\n`;
+    const title = `**${count++} - ${movie.title}**\n`;
+    const description = `**Descrição:** ${movie.overview}\n`;
+    const vote = `**Nota TMDB:** ${movie.voteAverage}\n`;
 
     listMessages.push(
       `${title}${movie.overview && movie.overview !== '' ? description : ''}${vote}${movie.teaser}\n\n`
